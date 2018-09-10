acbc44207c76669d0fb72133675ad7ece6ae24d6
Jonas Jevnaker Aas <jonasjaas@gmail.com>
2018-09-10 11:08:05 +0200

    Issue 1 is done. Did some minor cleanup

--- intexpo/src/index.js
+++ intexpo/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import logo from "./logo.svg";
-import logo2 from "./dickbutt.jpg";
 
 class Test extends React.Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo2} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to intexpo. The world of oppotunities!</h1>
                 </header>
                 <p className="App-intro">
