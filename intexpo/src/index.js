11b1f6b0c2b26ad15ac535a3382a39b0487bab71
Jonas Jevnaker Aas <jonasjaas@gmail.com>
2018-09-10 11:07:01 +0200

    Issue #1 is done. Did some minor cleanup

--- intexpo/src/index.js
+++ intexpo/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import logo from "./logo.svg";
+import logo2 from "./dickbutt.jpg";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Test extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo2} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to intexpo. The world of oppotunities!</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'));
 registerServiceWorker();
