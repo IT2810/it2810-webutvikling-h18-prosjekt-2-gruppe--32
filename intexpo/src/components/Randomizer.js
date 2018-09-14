2ddb5838a11b34be72a25fb5031ff766b74ee62a
svenngronbeck <srgronbe@stud.ntnu.no>
2018-09-14 09:56:34 +0200

    Updated Randomizer.js

--- intexpo/src/components/Randomizer.js
+++ intexpo/src/components/Randomizer.js
@@ -1,91 +1,94 @@
 import React from 'react';
 import '../style.css';
+import AjaxHandler from './ajaxHandler.js';
 
 class Randomizer extends React.Component {
 
     //Must take in which categories are selected, as a prop
     //This is an example
-    const selectedCategories = ['Abstrakt', 'Musikk', 'Trump-quotes'];
-    //const selectedCategories = [{this.props.selectedImage}, {this.props.selectedSound}, {this.props.selectedText}];
+    var selectedCategories = ['Abstrakt', 'Musikk', 'Trump-quotes'];
+    //The below should work, if the dropdown menu passes calls the ranomizer with the selected categories as props
+    //var selectedCategories = [{this.props.selectedImage}, {this.props.selectedSound}, {this.props.selectedText}];
 
-    //Must have all files of all categories saved
-    //This is an example
-    const abstract = ['abstract1.svg', 'abstract2.svg', 'abstract3.svg', 'abstract4.svg'];
-    const animals = ['animal1.svg', 'animal2.svg', 'animal3.svg', 'animal4.svg'];
-    const cars = ['car1.svg', 'car2.svg', 'car3.svg', 'car4.svg']
-    const music = ['1.mp3', '2.mp3', '3.mp3', '4.mp3'];
-    const instrument = ['bagpipe.mp3', 'flute.mp3', 'harmonica.mp3', 'synth.mp3'];
-    const weather = ['beach.mp3', 'rain.mp3', 'thunder.mp3', 'wind.mp3'];
-    const trump = ['Trump-quote1.json', 'Trump-quote2.json', 'Trump-quote3.json', 'Trump-quote4.json'];
-    const language = ['French.json', 'German.json', 'Nynorsk.json', 'Spanish.json'];
-    const oneliners = ['One-liner1.json', 'One-liner2.json', 'One-liner3.json','One-liner4.json'];
+    //Must have list of all files of all categories saved
+    var abstract = ['abstract1.svg', 'abstract2.svg', 'abstract3.svg', 'abstract4.svg'];
+    var animals = ['animal1.svg', 'animal2.svg', 'animal3.svg', 'animal4.svg'];
+    var cars = ['car1.svg', 'car2.svg', 'car3.svg', 'car4.svg']
+    var music = ['1.mp3', '2.mp3', '3.mp3', '4.mp3'];
+    var instrument = ['bagpipe.mp3', 'flute.mp3', 'harmonica.mp3', 'synth.mp3'];
+    var weather = ['beach.mp3', 'rain.mp3', 'thunder.mp3', 'wind.mp3'];
+    var trump = ['Trump-quote1.json', 'Trump-quote2.json', 'Trump-quote3.json', 'Trump-quote4.json'];
+    var language = ['French.json', 'German.json', 'Nynorsk.json', 'Spanish.json'];
+    var oneliners = ['One-liner1.json', 'One-liner2.json', 'One-liner3.json','One-liner4.json'];
 
     //Must then select four unique combinations based on the selected categories
-    const combo1 = [];
-    const combo2 = [];
-    const combo3 = [];
-    const combo4 = [];
-    const combos = [this.combo1, this.combo2, this.combo3, this.combo4];
+    var combo1 = [];
+    var combo2 = [];
+    var combo3 = [];
+    var combo4 = [];
+    var combos = [this.combo1, this.combo2, this.combo3, this.combo4];
     //Get selected categories
     //Images
     if(this.selectedCategories[0]=="Abstrakt"){
-      const imageCategory = this.abstract;
+      var imageCategory = this.abstract.slice(0);
     }
     else if(this.selectedCategories[0]=="Dyr"){
-      const imageCategory = this.animals;
+      var imageCategory = this.animals.slice(0);
     }
     else if(this.selectedCategories[0]=="Biler"){
-      const imageCategory = this.cars;
+      var imageCategory = this.cars.slice(0);
     }
 
     //Sounds
     if(this.selectedCategories[1]=="Instrument"){
-      const soundCategory = this.instrument;
+      var soundCategory = this.instrument.slice(0);
     }
     else if(this.selectedCategories[1]=="Musikk"){
-      const soundCategory = this.music;
+      var soundCategory = this.music.slice(0);
     }
     else if(this.selectedCategories[1]=="Vær"){
-      const soundCategory = this.weather;
+      var soundCategory = this.weather.slice(0);
     }
 
     //Text
     if(this.selectedCategories[2]=="Språk"){
-      const textCategory = this.language;
+      var textCategory = this.language.slice(0);
     }
     else if(this.selectedCategories[2]=="One-liners"){
-      const textCategory = this.oneliners;
+      var textCategory = this.oneliners.slice(0);
     }
     else if(this.selectedCategories[2]=="Trump-quotes"){
-      const textCategory = this.trump;
+      var textCategory = this.trump.slice(0);
     }
 
     //Put random elements in different combinations
-    for(x=0; x<5; <++){
+    for(x=0; x<4; <++){
       //Find and add random image to combo
-      const randomImageIndex = Math.floor(Math.random()*this.imageCategory.length);
-      const image1 = this.imageCategory[this.randomImageIndex];
+      var randomImageIndex = Math.floor(Math.random()*this.imageCategory.length);
+      var image1 = this.imageCategory[this.randomImageIndex];
       this.combos[x].push(this.image1);
       this.imageCategory.splice(this.randomImageIndex, 1);
 
       //Find and add random sound to combo
-      const randomSoundIndex = Math.floor(Math.random()*this.soundCategory.length);
-      const sound1 = this.soundCategory[this.randomSoundIndex];
+      var randomSoundIndex = Math.floor(Math.random()*this.soundCategory.length);
+      var sound1 = this.soundCategory[this.randomSoundIndex];
       this.combos[x].push(this.sound1);
       this.soundCategory.splice(this.randomSoundIndex, 1);
 
       //Find and add random text to combo
-      const randomTextIndex = Math.floor(Math.random()*this.textCategory.length);
-      const text1 = this.textCategory[this.randomTextIndex];
-      this.combos[x].push(this.text);
+      var randomTextIndex = Math.floor(Math.random()*this.textCategory.length);
+      var text1 = this.textCategory[this.randomTextIndex];
+      this.combos[x].push(this.text1);
       this.textCategory.splice(this.randomTextIndex, 1);
     }
 
-    //Must then pass on these four combinations to the ajax component
+    //Must then pass on these four combinations to the ajax component as props
 
     render(){
         return (
-            <div></div>
+            <AjaxHandler /* Pass combos-list as props here. */ />
+            //Example:
+            <AjaxHandler combo1=this.combo[0] combo2=this.combo[1] combo3=this.combo[2] combo4=this.combo[3] />
         )
     }
 }
