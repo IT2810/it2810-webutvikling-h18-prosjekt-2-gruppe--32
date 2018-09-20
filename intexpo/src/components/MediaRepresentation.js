3415aa06b2a17e602d906eb3c35a20c9f9245b82
Jonas Jevnaker Aas <jonasjaas@gmail.com>
2018-09-20 15:59:46 +0200

    Removed id from audio tag. It wasnt doing anything

--- intexpo/src/components/MediaRepresentation.js
+++ intexpo/src/components/MediaRepresentation.js
@@ -145,7 +145,7 @@ class MediaRepresentation extends React.Component {
                       </section>
 
                       <section id="mediaAudioContainer">
-                          <audio id="player" ref="audio" loop controls>
+                          <audio ref="audio" loop controls>
                               <source src={this.state.audio} type= "audio/mp3"/>
                               Your browser does not support the audio element
                           </audio>
@@ -164,6 +164,5 @@ class MediaRepresentation extends React.Component {
     )
   }
 }
-
 
 export default MediaRepresentation;
